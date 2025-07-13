3f806166778ce237e170b530a542f757a69f9183
assilbekov <askhat.assilbekov@gmail.com>
2025-07-13 19:06:54 +0500

    add pointer

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,15 +1,31 @@
 import { Button } from "@/components/Button";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import { Pointer } from "@/components/Pointer";
 
 export default function Hero() {
   return (
-    <section className="py-24">
-      <div className="container">
+    <section className="py-24 overflow-x-clip">
+      <div className="container relative">
+        <div className="absolute -left-32 top-16">
+          <Image src={designExample1Image} alt="Design Example 1" />
+        </div>
+        <div className="absolute -right-64 -top-16">
+          <Image src={designExample2Image} alt="Design Example 2" />
+        </div>
+        <div className="absolute -left-32 -bottom-32">
+          <Pointer name="Andrea" color="blue" />
+        </div>
+        <div className="absolute -right-32 -bottom-32">
+          <Pointer name="John" color="red" />
+        </div>
         <div className="flex justify-center">
           <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
             $7.5M Seed Round Raised
           </div>
         </div>
-        <h1 className="text-center text-6xl md:text-7xl font-medium mt-6">
+        <h1 className="text-center text-6xl md:text-7xl lg:text-8xl font-medium mt-6">
           Impactful desing, created effortlesly.
         </h1>
         <p className="text-center text-xl text-white/50 mt-8 max-w-2xl">
