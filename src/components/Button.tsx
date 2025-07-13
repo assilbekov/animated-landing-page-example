e75a8d444d15ed3fe6a1ec7a3bd9b59068d67c05
assilbekov <askhat.assilbekov@gmail.com>
2025-07-13 18:24:16 +0500

    add button

---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+import { HTMLAttributes } from "react";
+import { cva } from "class-variance-authority";
+
+const buttonVariants = cva("border h-12 rounded-full px-6 font-medium", {
+  variants: {
+    variant: {
+      primary: "bg-lime-400 text-neutral-950 border-lime-400",
+      secondary: "border-white",
+    },
+  },
+});
+
+export const Button = (
+  props: {
+    variant: "primary" | "secondary";
+  } & HTMLAttributes<HTMLButtonElement>
+) => {
+  return (
+    <button
+      {...props}
+      className={buttonVariants({
+        variant: props.variant,
+        className: props.className,
+      })}
+    >
+      {props.children}
+    </button>
+  );
+};
