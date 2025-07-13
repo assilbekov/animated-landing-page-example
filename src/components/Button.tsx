d8a69c93385cb45d49226956c43684eab25033fe
assilbekov <askhat.assilbekov@gmail.com>
2025-07-13 18:53:14 +0500

    pause

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from "react";
+import { ButtonHTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
 const buttonVariants = cva("border h-12 rounded-full px-6 font-medium", {
@@ -7,19 +7,24 @@ const buttonVariants = cva("border h-12 rounded-full px-6 font-medium", {
       primary: "bg-lime-400 text-neutral-950 border-lime-400",
       secondary: "border-white",
     },
+    size: {
+      sm: "h-10",
+    },
   },
 });
 
 export const Button = (
   props: {
     variant: "primary" | "secondary";
-  } & HTMLAttributes<HTMLButtonElement>
+    size?: "sm";
+  } & ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
   return (
     <button
       {...props}
       className={buttonVariants({
         variant: props.variant,
+        size: props.size,
         className: props.className,
       })}
     />
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,37 @@
+import { Button } from "@/components/Button";
+
 export default function Hero() {
-    return <div>Hero</div>;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div className="flex justify-center">
+          <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+            $7.5M Seed Round Raised
+          </div>
+        </div>
+        <h1 className="text-center text-6xl font-medium mt-6">
+          Impactful desing, created effortlesly.
+        </h1>
+        <p className="text-center text-xl text-white/50">
+          Design tools shouldn&apos;t slow you down. Layers powerful features
+          with an intuitive interface that&apos;s a breeze to use.
+        </p>
+        <form className="flex border border-white/15 rounded-full p-2 mt-8">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="bg-transparent px-4"
+          />
+          <Button
+            variant="primary"
+            type="submit"
+            size="sm"
+            className="whitespace-nowrap"
+          >
+            Get Started
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
 }
