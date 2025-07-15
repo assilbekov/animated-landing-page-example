19cdb7aaa473a0b268a96618d85e0eecbdbbcf2f
assilbekov <askhat.assilbekov@gmail.com>
2025-07-15 20:00:14 +0500

    finish footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,36 @@
+import logoImage from "@/assets/images/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+
 const footerLinks = [
-    { href: "#", label: "Contact" },
-    { href: "#", label: "Privacy Policy" },
-    { href: "#", label: "Terms & Conditions" },
+  { href: "#", label: "Contact" },
+  { href: "#", label: "Privacy Policy" },
+  { href: "#", label: "Terms & Conditions" },
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+  return (
+    <footer className="py-16">
+      <div className="container">
+        <div className="flex flex-col md:flex-row justify-between gap-6 items-center">
+          <div>
+            <Image src={logoImage} alt="Logo" />
+          </div>
+          <div>
+            <nav className="flex gap-6">
+              {footerLinks.map((link) => (
+                <Link
+                  key={link.href}
+                  href={link.href}
+                  className="text-white/50 hover:text-white transition-colors text-sm"
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
