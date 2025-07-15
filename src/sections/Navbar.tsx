9a523543e2b7a144f3c0e712890dbe336346f9c5
assilbekov <askhat.assilbekov@gmail.com>
2025-07-15 22:16:39 +0500

    finish navbar mobile animations

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/Button";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
 
 const navLinks = [
   { label: "Home", href: "#" },
@@ -18,7 +19,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <section className="py-4 lg:py-8 sticky top-0 z-50">
+    <section className="py-4 lg:py-8 fixed w-full top-0 z-50">
       <div className="container max-w-5xl">
         <div className="border border-white/15 rounded-[27px] bg-neutral-950/70 backdrop-blur">
           <div className="grid grid-cols-2 lg:grid-cols-3 p-2 items-center px-4 md:pr-2">
@@ -90,21 +91,30 @@ export default function Navbar() {
               </Button>
             </div>
           </div>
-          {isOpen && (
-            <div className="md:hidden flex flex-col items-center gap-4 py-2">
-              {navLinks.map((link) => (
-                <Link key={link.label} href={link.href} className="">
-                  {link.label}
-                </Link>
-              ))}
-              <Button variant="secondary" className="w-full">
-                Log in
-              </Button>
-              <Button variant="primary" className="w-full">
-                Sign Up
-              </Button>
-            </div>
-          )}
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                initial={{ height: 0 }}
+                animate={{ height: "auto" }}
+                exit={{ height: 0 }}
+                className="overflow-hidden"
+              >
+                <div className="flex flex-col items-center gap-4 py-2">
+                  {navLinks.map((link) => (
+                    <Link key={link.label} href={link.href} className="">
+                      {link.label}
+                    </Link>
+                  ))}
+                  <Button variant="secondary" className="w-full">
+                    Log in
+                  </Button>
+                  <Button variant="primary" className="w-full">
+                    Sign Up
+                  </Button>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </section>
